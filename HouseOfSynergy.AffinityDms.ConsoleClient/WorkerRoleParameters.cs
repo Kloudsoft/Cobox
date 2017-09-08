e87e531cef4e1e7146c9f51967704eea9c3ad399
Manju <manjushree.nl@kloud-soft.com>
2017-09-08 08:47:43 +0530

    33

---
+++ HouseOfSynergy.AffinityDms.ConsoleClient/WorkerRoleParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HouseOfSynergy.AffinityDms.OcrLibrary;
+
+namespace HouseOfSynergy.AffinityDms.ConsoleClient
+{
+	public sealed class WorkerRoleParameters
+	{
+		public OcrEngineSettings OcrEngineSettings { get; private set; }
+		public CancellationToken CancellationToken { get; private set; }
+
+		public WorkerRoleParameters (OcrEngineSettings ocrEngineSettings, CancellationToken cancellationToken)
+		{
+			if (ocrEngineSettings == null) { throw (new ArgumentNullException("ocrEngineSettings")); }
+			if (cancellationToken == null) { throw (new ArgumentNullException("cancellationToken")); }
+
+			this.OcrEngineSettings = ocrEngineSettings;
+			this.CancellationToken = cancellationToken;
+		}
+	}
+}
